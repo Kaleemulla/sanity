85b9bc5a2fd65d39bd9122f4211a230e919439ef
Kaleemulla Sharief <kalimullasharief@gmail.com>
2016-11-06 21:12:39 +0530

    Modifed

--- remote/connection.py
+++ remote/connection.py
@@ -13,7 +13,7 @@ def ssh_collect(server, user, port, timeout, command):
                 stdin, stdout, stderr = ssh.exec_command(command)
                 return (stdin.readlines, stdout.readlines(), stderr.readlines(), ssh)
         except paramiko.AuthenticationException, e:
-                return ("", "Error1", "", ssh)
+                return ("", "Error1", "Default Password Not Working", ssh)
         except socket.error, e:
-                return ("", "Error2", "", ssh)
+                return ("", "Error2", "Server Not Found", ssh)
 
