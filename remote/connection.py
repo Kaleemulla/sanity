00fbfb420cb6671246d99872bab32f93b30b1f6b
Kaleemulla Sharief <Kalimullasharief@gmail.com>
2016-11-06 20:34:31 +0530

    Create connection.py

---
+++ remote/connection.py
@@ -0,0 +1,19 @@
+import socket
+import paramiko
+
+def ssh_collect(server, user, port, timeout, command):
+        if (user == "em7admin"):
+                passwd = "em7admin"
+        else:
+                passwd = "ast4roslab"
+        try:
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh.connect(server, username=user, password=passwd, port=int(port), timeout=timeout)
+                stdin, stdout, stderr = ssh.exec_command(command)
+                return (stdin.readlines, stdout.readlines(), stderr.readlines(), ssh)
+        except paramiko.AuthenticationException, e:
+                return ("", "Error1", "", ssh)
+        except socket.error, e:
+                return ("", "Error2", "", ssh)
+
